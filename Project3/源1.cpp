2bfee38fcd828ffb09e275458db6bcc7b6233469
27707 <27707@DELL-3991>
2022-11-15 23:37:33 +0800

    测试2

--- Project3/源1.cpp
+++ Project3/源1.cpp
@@ -1,8 +1,8 @@
-#删除原来远程仓库（origin表示远程仓库名称，如果不知道可以使用git remote - v 命令查看）
-git remote rm origin
-
-#添加远程仓库地址（记得选择git方式的远程仓库地址）
-git remote add origin git@github.com:ClowLAY / spring - cloud - config.git
-
-#最后再次提交项目
-git push - u origin main
+//#删除原来远程仓库（origin表示远程仓库名称，如果不知道可以使用git remote - v 命令查看）
+//git remote rm origin
+//
+//#添加远程仓库地址（记得选择git方式的远程仓库地址）
+//git remote add origin git@github.com:ClowLAY / spring - cloud - config.git
+//
+//#最后再次提交项目
+//git push - u origin main
