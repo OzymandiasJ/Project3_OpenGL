dc43d6575de3f1640c2d8f66ee0a84dbc73d6d48
27707 <27707@DELL-3991>
2022-11-15 23:40:59 +0800

    测试4

--- Project3/源1.cpp
+++ Project3/源1.cpp
@@ -1,8 +1,16 @@
-//#删除原来远程仓库（origin表示远程仓库名称，如果不知道可以使用git remote - v 命令查看）
-//git remote rm origin
-//
-//#添加远程仓库地址（记得选择git方式的远程仓库地址）
-//git remote add origin git@github.com:ClowLAY / spring - cloud - config.git
-//
-//#最后再次提交项目
-//git push - u origin main
+#include <stdio.h>
+int main()
+{
+	int x, y;
+	printf("请输入x的值：");
+	scanf_s("%d", &x);
+	if (x < 5)
+		y = 2 * x + 1;
+	else if (x < 8)
+		y = 5 * x - 5;
+	else
+		y = 8 * x - 3;
+	printf("x的值为：%d,y的值为:%d\n", x, y);
+	return 0;
+
+}
